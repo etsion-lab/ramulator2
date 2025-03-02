e29fc22b3576c6e169da209bc8f70aa8d046daa0
Yoav Etsion <yetsion@technio.ac.il>
2025-03-02 18:46:55 +0200

    general run script

---
+++ scripts/run.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use experimental 'smartmatch';
+
+use Data::Dumper;
+
+my $BENCHDIR="/scratch/yetsion/cows/cloudsuite-traces";
+my $BINARY="/scratch/yetsion/cows/tools/ramulator2/build/ramulator2";
+
+my $RESDIR="/scratch/yetsion/cows/tools/ramulator2/res";
+
+my %BENCHMARKS = (
+    "data-analytics-core" =>    { "cores" => [ 1, 8 ],  "half" => 0 },
+    "data-caching-core" =>      { "cores" => [ 1 ],     "half" => 1 },
+    "data-serving-core" =>      { "cores" => [ 1 ],     "half" => 1 },
+    "graph-analytics-core" =>   { "cores" => [ 1, 8 ],  "half" => 0 },
+    "in-memory-analytics-core" => { "cores" => [ 1, 8 ], "half" => 0 },
+    "media-streaming-core" =>   { "cores" => [ 1 ],     "half" => 1 },
+    "web-search-core" =>        { "cores" => [ 1 ],     "half" => 1 },
+    "web-serving-core" =>       { "cores" => [ 1 ],     "half" => 1 },
+);
+
+#
+# Helpers
+#
+sub find_bench($) {
+    my $b = shift;
+
+    if($b !~ /^(.+)-(\d)$/) {
+        die "Cannot parse benchmark name \"$b\"";
+    }
+    my $bnick = $1;
+    my $cores = $2;
+
+    my $bname = undef;
+    foreach my $bn (keys(%BENCHMARKS)) {
+        if(index($bn, $bnick) == 0) {
+            if(defined($bname)) {
+                die "benchmark nickname \"$bnick\" is not unique";
+            }
+            if(!($cores ~~ $BENCHMARKS{$bn}{"cores"})) {
+                die "benchmark \"$bn\" does not support $cores cores"
+            }
+            $bname = $bn;
+        }
+    }
+
+    return ($bname, $cores, $BENCHMARKS{$bname});
+}
+
+sub bench_to_trace($$$$) {
+    (my $name, my $cores, my $d, my $asid) = @_;
+
+    my $start_core = 0;
+    $start_core = $cores if($d->{"half"});
+
+    my @ret;
+    for ($start_core .. ($start_core + $cores - 1)) {
+        my $c = $_;
+
+        push(@ret, "- $BENCHDIR/$name-$cores/trace.cpu$c:$asid");
+    }
+
+    @ret;
+}
+
+sub build_trace_list {
+    my @benchs = @_;
+    my @ret;
+
+    my $asid = 0;
+    for my $b (@benchs) {
+        (my $bname, my $c, my $d) = find_bench($b);
+
+        push(@ret, bench_to_trace($bname, $c, $d, $asid));
+        $asid++;
+    }
+
+    @ret;
+}
+
+sub gen_config($$$) {
+    (my $in, my $out, my $map) = @_;
+
+    print("in=$in, out=$out\n");
+
+    open(my $fin, '<', $in) or die "Could not open input config file $in: $!";
+    open(my $fout, '>', $out) or die "Could not open input config file $out: $!";
+
+    while(<$fin>) {
+        chomp;
+        next if(/^\s*\#/); # skip comments
+
+        my $line = $_;
+
+        # find macros
+        if($line =~ /^(\s*)([A-Z_]+[0-9]*)\s*$/) {
+            my $s = $1;
+            my $macro = $2;
+
+            die "Error: undefined macro \"$macro\"" if(!defined($map->{$macro}));
+            my @l = @{$map->{$macro}};
+            foreach (@l) {
+                print $fout "$s" . $_ . "\n";
+            }
+        }
+        else {
+            print $fout "$line\n";
+        }
+    }
+
+    close($fin);
+    close($fout);
+}
+
+#
+# Main
+#
+my @benchs;
+my $test_name;
+my $confile;
+
+GetOptions ("bench=s" => \@benchs,
+            "name=s" => \$test_name,
+            "config=s" => \$confile)
+    or die("Error in command line arguments\n");
+
+die "Error: no benchmarks given" if(scalar(@benchs) == 0);
+die "Error: no test name specified" if(!defined($test_name));
+die "Error: no config file specified" if(!defined($confile));
+
+my %macros;
+@{$macros{"TRACES"}} = build_trace_list(@benchs);
+
+# create rundir
+my $rundir = "$RESDIR/$test_name";
+system("mkdir -p $rundir");
+
+# create config file
+my $outconf = "$rundir/config";
+gen_config($confile, $outconf, \%macros);
+
+system("cd $rundir && /usr/bin/time -v $BINARY -f $outconf > out");
