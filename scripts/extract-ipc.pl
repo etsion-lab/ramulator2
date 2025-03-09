40263d1c7c8eda8cc8179877e6051bcfcc983ef6
Yoav Etsion <yetsion@technio.ac.il>
2025-03-09 18:49:30 +0200

    parse multiple output files

--- scripts/extract-ipc.pl
+++ scripts/extract-ipc.pl
@@ -8,26 +8,35 @@ my $ninsts = 0;
 my %cycles_per_core;
 my $total_cycles = 0;
 
-while(<>) {
-    chomp;
-    next if(/^\s*\#/);
+my @files = @ARGV;
 
-    if(/^\s*cycles_recorded_core_(\d+):\s+(\d+)\s*$/) {
-        my $core = int($1);
-        my $cycles = int($2);
-        $cycles_per_core{$core} = $cycles;
-        $total_cycles += $cycles;
-    }
-    elsif(/num_expected_insts:\s+(\d+)\s*$/) {
-        $ninsts = $1;
-    }
-}
+foreach my $f (@files) {
+    my @p = split(/\//, $f);
+    my $name = $p[scalar(@p) - 2];
 
-my $t=0;
-my $ncores = scalar(keys %cycles_per_core);
-for(my $c=0; $c<$ncores; $c++) {
-    my $cy = $cycles_per_core{$c};
-    printf("IPC[%3d]=%4.2f\n", $c, ($ninsts/$cy));
+    open(my $fd, "<$f") or die "Error openning file $f: $!";
+
+    while(<$fd>) {
+        chomp;
+        next if(/^\s*\#/);
+
+        if(/^\s*cycles_recorded_core_(\d+):\s+(\d+)\s*$/) {
+            my $core = int($1);
+            my $cycles = int($2);
+            $cycles_per_core{$core} = $cycles;
+            $total_cycles += $cycles;
+        }
+        elsif(/num_expected_insts:\s+(\d+)\s*$/) {
+            $ninsts = $1;
+        }
+    }
+
+    my $t=0;
+    my $ncores = scalar(keys %cycles_per_core);
+    for(my $c=0; $c<$ncores; $c++) {
+        my $cy = $cycles_per_core{$c};
+#        printf("IPC[%3d]=%4.2f\n", $c, ($ninsts/$cy));
+    }
+    printf("%-30s: IPC[Total]=%.2f # (Total insts: %d, Total cycles: %d)\n",
+            $name, (($ninsts*$ncores)/$total_cycles), ($ninsts*$ncores), $total_cycles);
 }
-printf("IPC[Total]=%.2f # (Total insts: %d, Total cycles: %d)\n",
-        (($ninsts*$ncores)/$total_cycles), ($ninsts*$ncores), $total_cycles);
