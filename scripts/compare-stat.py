a3c112f3a83f898e5cd136f1be1d968a1eaa2894
Yoav Etsion <yetsion@technio.ac.il>
2026-03-10 17:19:22 +0200

    script can now support multiple param values

--- scripts/compare-stat.py
+++ scripts/compare-stat.py
@@ -78,6 +78,7 @@ def main(argv: list[str]) -> int:
     parser.add_argument("-s", "--size", help="Benchmark size filter")
     parser.add_argument("-c", "--cores", help="Core count filter")
     parser.add_argument("-t", "--base", help="Core count filter")
+    parser.add_argument("-n", "--nvals", help="Values for N parameter, comma-separated")
 
     args = parser.parse_args(argv)
 
@@ -95,37 +96,57 @@ def main(argv: list[str]) -> int:
         print("Error: test name cannot be empty.", file=sys.stderr)
         return 1
 
-
-    benchs = [f for f in os.listdir(BASEDIR + "/" + testname) if f.endswith('-8') or f.endswith('-1')]
+    if args.nvals is not None:
+        nvals = [v.strip() for v in args.nvals.split(",") if v.strip()]
+    else:
+        nvals = [ "UNSET" ]
 
     base_path = Path(BASEDIR) / BASETST
-    bench_path = Path(BASEDIR) / testname
+    bench_path_raw = Path(BASEDIR) / testname
+    print(f" statname: {statname}\n testname: {testname}\n base_path: {base_path}\n bench_path: {bench_path_raw}\n N={nvals}")
 
-    print(f" statname: {statname}\n testname: {testname}\n base_path: {base_path}\n bench_path: {bench_path}")
+    random_bench_dirname = testname.replace("=N", f"={nvals[0]}") if nvals else testname
+    benchs = [f for f in os.listdir(f"{BASEDIR}/{random_bench_dirname}") if f.endswith('-8') or f.endswith('-1')]
 
     for b in benchs:
-        base_file = base_path / b / "out"
-        bench_file = bench_path / b / "out"
+        if(args.nvals is not None):
+            print(f"\n\nComparing stats for benchmark {b}...")
 
         print(f"processing {b}...\r", end="", flush=True)
 
+        # read db for baseline
+        base_file = base_path / b / "out"
         base_dict = load_ramulator_yaml(str(base_file))
-        bench_dict = load_ramulator_yaml(str(bench_file))
-        if base_dict is None or bench_dict is None:
-            print(f"Missing stats for benchmark {b}.")
+        if base_dict is None:
+            print(f"Missing base stats for benchmark {b} ({base_file}).")
             continue
 
         if 'ipc_total' not in base_dict.get('Frontend', {}):
             compute_ipc(base_dict)
-        if 'ipc_total' not in bench_dict.get('Frontend', {}):
-            compute_ipc(bench_dict)
-
         base_stat=base_dict['Frontend'].get(statname, 1)
-        bench_stat=bench_dict['Frontend'].get(statname)
-#        print(f"bench {b}: Base {statname}: {base_stat}, Bench {statname}: {bench_stat}")
-        ratio = bench_stat / base_stat if base_stat != 0 else float('inf')
-        print(f"{b:<30} bench/base: {ratio:12.3f} (base: {base_stat}, bench: {bench_stat})")
 
+        # go through the requested values
+        for n in nvals:
+            formatted_testname = testname.replace("=N", f"={n}")
+            bench_dirname = f"{BASEDIR}/{formatted_testname}"
+            bench_path = Path(bench_dirname)
+
+            bench_file = bench_path / b / "out"
+            bench_dict = load_ramulator_yaml(str(bench_file))
+            if bench_dict is None:
+                print(f"Missing bench stats for benchmark {b} ({bench_file}).")
+                continue
+
+            if 'ipc_total' not in bench_dict.get('Frontend', {}):
+                compute_ipc(bench_dict)
+
+            bench_stat=bench_dict['Frontend'].get(statname)
+    #        print(f"bench {b}: Base {statname}: {base_stat}, Bench {statname}: {bench_stat}")
+            ratio = bench_stat / base_stat if base_stat != 0 else float('inf')
+            if(args.nvals is not None):
+                print(f"{n:<30} bench/base: {ratio:12.3f} (base: {base_stat}, bench: {bench_stat})")
+            else:
+                print(f"{b:<30} bench/base: {ratio:12.3f} (base: {base_stat}, bench: {bench_stat})")
 
 
 if __name__ == "__main__":
