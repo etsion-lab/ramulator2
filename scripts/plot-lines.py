ab7c0f9b8713c4317637af901b416c6382629883
Yoav Etsion <yetsion@technio.ac.il>
2026-03-24 18:46:18 +0200

    rewrite script that plots lines

--- scripts/plot-lines.py
+++ scripts/plot-lines.py
@@ -1,81 +1,91 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
 import argparse
+import re
+import os
 import numpy as np
-import random
+import matplotlib.pyplot as plt
 
-def col_load(file, col_idx):
-    ret = np.loadtxt(file, dtype=float, usecols=col_idx, comments='#')
-    return ret
 
-def col_sample(nsamples, col):
-    if(nsamples == 0):
-        return col
+def parse_k(value):
+    parts = value.split(',')
+    if len(parts) != 2:
+        raise argparse.ArgumentTypeError("-k must be in x,y format (e.g. -k 1,2)")
+    try:
+        x, y = int(parts[0]), int(parts[1])
+    except ValueError as exc:
+        raise argparse.ArgumentTypeError("-k values must be integers") from exc
+    if x < 1 or y < 1:
+        raise argparse.ArgumentTypeError("-k column indices are 1-based and must be >= 1")
+    return (x, y)
 
-    first=col[0]
-    last=col[-1]
-    col=np.delete(col, [0, len(col)-1])
-    nsamples = nsamples - 2
 
-    ret=[]
-    ret.append(first)
-    for l in np.array_split(col, nsamples):
-        ret.append(random.sample(l.tolist(), 1)[0])
-    ret.append(last)
+def extract_label(filepath, pattern):
+    if pattern is None:
+        return os.path.basename(filepath)
+    m = re.search(pattern, filepath)
+    if m and m.lastindex and m.lastindex >= 1:
+        return m.group(1)
+    return os.path.basename(filepath)
 
-    return np.array(ret)
 
-def build_parser():
-    parser = argparse.ArgumentParser(
-                        prog='plotlines',
-                        description='plot lines from columnar files')
+def load_xy(filepath, xcol, ycol, label):
+    print(f"Reading X axis for '{label}'...")
+    xs = np.loadtxt(filepath, comments='#', usecols=(xcol - 1,))
+    print(f"Reading Y axis for '{label}'...")
+    ys = np.loadtxt(filepath, comments='#', usecols=(ycol - 1,))
+    if xs.ndim == 0:
+        xs, ys = xs.reshape(1), ys.reshape(1)
+    return xs, ys
 
-    parser.add_argument('-s', '--samples',
-                        type=int,
-                        default=0,
-                        help="how many samples to use from the array")
-    parser.add_argument('-f', '--file',
-                        required=True,
-                        type=argparse.FileType('r'),
-                        #nargs='+',
-                        action='append',
-                        help="file to read from")
-    parser.add_argument('-c', '--col',
-                        required=True,
-                        type=int,
-                        help="column to plot (starting at 0)")
-    parser.add_argument('-o', '--output',
-                        help="output file to save to")
 
-    return parser
-
-#
-# Main
-#
 def main():
+    parser = argparse.ArgumentParser(
+        prog='plot-lines',
+        description='Plot lines from columnar data files')
+    parser.add_argument('-k', '--key-cols',
+                        required=True,
+                        type=parse_k,
+                        metavar='x,y',
+                        help='1-based column indices for X and Y (e.g. -k 1,2)')
+    parser.add_argument('-l', '--legend-regex',
+                        default=None,
+                        metavar='PATTERN',
+                        help='regex with () group to extract legend label from file path')
+    parser.add_argument('-t', '--title',
+                        default=None,
+                        metavar='TITLE',
+                        help='plot title')
+    parser.add_argument('-xmin', type=float, default=None, metavar='XMIN')
+    parser.add_argument('-xmax', type=float, default=None, metavar='XMAX')
+    parser.add_argument('-ymin', type=float, default=None, metavar='YMIN')
+    parser.add_argument('-ymax', type=float, default=None, metavar='YMAX')
+    parser.add_argument('files',
+                        nargs='+',
+                        metavar='FILE',
+                        help='input data files')
 
-    parser = build_parser()
     args = parser.parse_args()
+    xcol, ycol = args.key_cols
 
-    xarray=[ float(v)/(args.samples/100) for v in range(args.samples+1) ]
+    for filepath in args.files:
+        label = extract_label(filepath, args.legend_regex)
+        xs, ys = load_xy(filepath, xcol, ycol, label)
+        plt.plot(xs, ys, label=label)
 
-    for f in args.file:
-        col = col_load(f.name, args.col)
-        yarray = col_sample(args.samples+1, col)
-        #yarray=[ 100.0*v for v in yarray ]
+    if args.title is not None:
+        plt.title(args.title)
 
-        plt.plot(xarray, yarray)
+    if args.xmin is not None or args.xmax is not None:
+        plt.xlim(left=args.xmin, right=args.xmax)
+    if args.ymin is not None or args.ymax is not None:
+        plt.ylim(bottom=args.ymin, top=args.ymax)
 
-    plt.xlabel("Run time [%]")
-    plt.ylabel("Fraction of entries in LLC needed\nto reqresent cached DRAM pages")
-    plt.yticks(np.arange(0.0, 1.01, 0.1))
-    plt.grid(visible=True, axis='y', which='both')
-    if args.output is not None:
-        print(f"Saving image to {args.output}")
-        plt.savefig(args.output)
-    else:
-        plt.show()
+    plt.grid(visible=True, axis='y')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
